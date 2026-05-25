63e56b505a89ceb65cc92045ae6491203a6d8189
putra-pkwl03 <putrapongkowulu@gmail.com>
2026-05-26 05:23:17 +0700

    deploimnet7

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar' // Sesuaikan dengan lokasi file Sidebar Anda
+import React, { Suspense } from 'react' // 1. Import Suspense dari React
+import Sidebar from '../components/Sidebar' 
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex min-h-screen bg-[#FDF9F5]">
-      {/* Panggil komponen Sidebar */}
-      <Sidebar />
+      {/* 2. Bungkus komponen Sidebar dengan Suspense */}
+      <Suspense fallback={<div className="w-64 bg-[#8B5E3C] min-h-screen animate-pulse" />}>
+        <Sidebar />
+      </Suspense>
 
-      {/* Wadah Content di Sebelah Kanan (wajib ada ml-64 agar tidak tertutup sidebar yang posisinya fixed) */}
+      {/* Wadah Content di Sebelah Kanan */}
       <main className="flex-1 min-h-screen p-6 ml-64">
         {children}
       </main>
